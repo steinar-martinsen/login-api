44cad0917f5cf1a6889963b1a3f9f9afa4698751
Steinar <steinar_martinsen@outlook.com>
2022-04-12 09:55:08 +0200

    testing login validation

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+const express = require("express");
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('login.js');
-const middlewares = jsonServer.defaults();
+// const router = jsonServer.router('login.js');
+// const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+const app = express();
 
-server.use(middlewares);
-server.use(router);
+app.use(express.json()); //parses incoming requests as JSON
+app.use("/", routes);
 
-server.listen(port);
+// server.use(middlewares);
+// server.use(router);
+
+//server.listen(port);
