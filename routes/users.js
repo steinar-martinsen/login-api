b07a15f4f62393392d7835ba4a2334be86140a22
Steinar <steinar_martinsen@outlook.com>
2022-04-12 11:16:39 +0200

    testing login validation

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const login = require('@/login.js');
+const data = require('@/db.json');
 
 
 router.get("/login",login.validateLogin);
+router.get("/data", data);
