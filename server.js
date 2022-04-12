2a0300219eb825f54dfb0c56eab30b5f72b65fc9
Steinar <steinar_martinsen@outlook.com>
2022-04-12 11:35:25 +0200

    testing login validation

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require("express");
-const jsonServer = require('json-server');
-const server = jsonServer.create();
+const routes = require("./routes/tea");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const cors = require("cors");
+const compression = require("compression");
+
+require("dotenv").config();
+
+// const express = require("express");
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
 // const router = jsonServer.router('login.js');
 // const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 const app = express();
 
+
+app.use(helmet());
+app.use(compression()); //Compress all routes
+
+app.use(cors({ origin: "https://ho1-login-api.herokuapp.com/" }));
+
 app.use(express.json()); //parses incoming requests as JSON
 app.use("/", routes);
+
+//Index page (static HTML)
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/index.html");
+});
+
 
 // server.use(middlewares);
 // server.use(router);
